0000446fc6ac7467a2f5320bbd8053ca7b8ddca8
Karol_VG <karolabdiel_vgf@hotmail.com>
2026-06-17 14:30:27 -0600

    #App.py v2

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Proyecto_7
+#Esta aplicacion sirve para poder visualizar graficos de la relacion entre precio y osometro, se seguira desarrollando para poder lograr interarar entre diferentes graficas segun desee el usuario
--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import plotly.graph_objects as go
 
 # lee el archivo CSV con los datos de vehículos
 car_data = pd.read_csv("vehicles_us.csv")
+st.title("Análisis de Datos de Vehículos")  # Título de la aplicación Streamlit
 
 # Crear un boton en Streamlit para mostrar el gráfico
 his_button = st.button("Construir Histograma")
@@ -20,4 +21,17 @@ if his_button:
     fig.update_layout(title_text='Distribución del Odómetro')
     # Mostrar el gráfico Plotly interactivo en la aplicación Streamlit
     # 'use_container_width=True' ajusta el ancho del gráfico al contenedor
+    st.plotly_chart(fig, use_container_width=True)
+
+scatter_button = st.button("Construir gráfico de dispersión")
+if scatter_button:
+    st.write("Construyendo el gráfico de dispersión...")
+    # Crear un scatter plot utilizando plotly.graph_objects
+    fig = go.Figure(data=go.Scatter(
+        x=car_data['odometer'], y=car_data['price'], mode='markers'))
+    # 'mode=markers' indica que queremos un gráfico de dispersión con puntos
+
+    # Añade un título al gráfico
+    fig.update_layout(title_text='Relación entre odómetro y precio')
+    # Mostrar el gráfico Plotly interactivo en la aplicación Streamlit
     st.plotly_chart(fig, use_container_width=True)
